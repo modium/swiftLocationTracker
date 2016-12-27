9e859b7d75ba1d8dfa8a92b022c7274e7873868e
Jaf Crisologo <rtql8d@gmail.com>
2016-12-27 07:27:03 -0700

    First commit

--- Waypoints/ViewController.swift
+++ Waypoints/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var meterLabel: UILabel!
+    @IBOutlet weak var kmLabel: UILabel!
+    @IBOutlet weak var mileLabel: UILabel!
+    
+    let meterThreshold = LocationTracker(threshold: 10.0)
+    let kmThreshold = LocationTracker(threshold: 1000.0)
+    let mileThreshold = LocationTracker(threshold: 1609.4)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateLocationLabel(withText: "Unknown")
+
+        meterThreshold.addLocationChangeObserver { (result) -> () in
+            switch result {
+            case .success(let location):
+                let coordinate = location.physical.coordinate
+                let locationString = "\(coordinate.latitude), \(coordinate.longitude)"
+                self.updateLocationLabel(withText: locationString)
+            case .failure:
+                self.updateLocationLabel(withText: "Failure")
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    func updateLocationLabel(withText text: String) -> Void {
+//        self.locationLabel.text = "Location: \(text)"
+        meterLabel.text = "Location: \(text)"
+        kmLabel.text = "Location: \(text)"
+        mileLabel.text = "Location: \(text)"
+    }    
 }
 
