22830a08d69beda851201a3fc4e9717ab67b63bb
Jaf Crisologo <rtql8d@gmail.com>
2017-11-05 14:10:41 -0700

    Added timer-based listener

--- Waypoints/ViewController.swift
+++ Waypoints/ViewController.swift
@@ -7,26 +7,38 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
 
     @IBOutlet weak var meterLabel: UILabel!
     @IBOutlet weak var kmLabel: UILabel!
     @IBOutlet weak var mileLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageLbl: UILabel!
     
-    let meterThreshold = LocationTracker(threshold: 10.0)
+    let manager = CLLocationManager()
+    
+    let meterThreshold = LocationTracker(threshold: 3.0)
     let kmThreshold = LocationTracker(threshold: 1000.0)
     let mileThreshold = LocationTracker(threshold: 1609.4)
     
+    var testArray = [String]()
     var kmWaypoints = [String]()
     var mileWaypoints = [String]()
-    var testArray = [String] ()
     
     let testBOL = "BOL-1016"
     
+    var timer = Timer()
+    
+    var counter = 0
+    var lat = [Double]()
+    var long = [Double]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        manager.requestWhenInUseAuthorization()
         
         testArray.removeAll() // Remove all elements from array
         
@@ -37,6 +49,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             case .success(let location):
                 let coordinate = location.physical.coordinate
                 let locationString = "\(coordinate.latitude), \(coordinate.longitude)"
+                
+                /* */
+                self.lat.append(coordinate.latitude)
+                self.long.append(coordinate.longitude)
+                self.counter += 1
+                /* */
                 
                 // Test function
                 self.updateLocation(latitude: "\(coordinate.latitude)", longitude: "\(coordinate.longitude)", bolID: self.testBOL)
@@ -66,11 +84,42 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 let coordinate = location.physical.coordinate
                 let locationString = "\(coordinate.latitude), \(coordinate.longitude)"
                 self.updateMileLabel(withText: locationString)
-//                self.mileWaypoints.append(locationString)
+                self.mileWaypoints.append(locationString)
             case .failure:
                 self.updateMileLabel(withText: "Failure")
             }
         }
+        
+        // Automatically reload table
+        timer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(movedThreeMeters), userInfo: nil, repeats: true)
+//        timer.invalidate()
+        
+    }
+    
+    /* Check if you've moved 3m from last position */
+    func movedThreeMeters() {
+        if (testArray.count < 2) {
+            return
+        }
+        
+        let lastPosition = CLLocation(latitude: lat[counter - 2], longitude: long[counter - 2])
+        let currentPosition = CLLocation(latitude: lat[counter - 1], longitude: long[counter - 1])
+        let distanceInMeters = roundDistance(meters: currentPosition.distance(from: lastPosition))
+        print("Current location is \(distanceInMeters)m from destination")
+        
+        if (distanceInMeters <= 3) {
+            print("You never moved")
+            messageLbl.text = "You never moved"
+        } else {
+            print("Moved 3 meters")
+            messageLbl.text = "Moved 3 meters"
+            self.tableView.reloadData()
+        }
+    }
+    
+    func roundDistance(meters:Double) -> Int {
+        let distance = meters.rounded()
+        return Int(distance)
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -100,6 +149,19 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.reloadData()
     }
     
+    func updateLocationLabel(withText text: String) -> Void {
+        meterLabel.text = "Location: \(text)"
+    }
+    
+    func updateKMLabel(withText text: String) -> Void {
+        kmLabel.text = "Location: \(text)"
+    }
+    
+    func updateMileLabel(withText text: String) -> Void {
+        mileLabel.text = "Location: \(text)"
+    }
+    
+    
     func updateLocation(latitude: String, longitude: String, bolID: String) {
         print("'lat': \(latitude), 'long': \(longitude), 'bol_id': \(bolID)")
     
@@ -110,7 +172,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 //        let postString = "email=\(userEmailAddress!)&password=\(userPassword!)";
 //        
 //        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding);
-//        
+//
 //        NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data:NSData?, response:NSURLResponse?, error:NSError?) in
 //            
 //            dispatch_async(dispatch_get_main_queue()) {
@@ -170,16 +232,9 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
-    func updateLocationLabel(withText text: String) -> Void {
-        meterLabel.text = "Location: \(text)"
+    
+    @IBAction func stopBtnPressed(_ sender: Any) {
+        
     }
 
-    func updateKMLabel(withText text: String) -> Void {
-        kmLabel.text = "Location: \(text)"
-    }
-
-    func updateMileLabel(withText text: String) -> Void {
-        mileLabel.text = "Location: \(text)"
-    }
 }
